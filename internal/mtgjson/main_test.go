d58255a43b80582b6d23b803354032260c826c48
Konstantin Förster <k.georgiew@gmail.com>
2025-03-23 16:19:13 +0100

    only warn logs for tests

--- internal/mtgjson/main_test.go
+++ internal/mtgjson/main_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestMain(m *testing.M) {
 	logger.SetupConsoleLogger()
-	err := logger.SetLogLevel("debug")
+	err := logger.SetLogLevel("warn")
 	if err != nil {
 		fmt.Printf("Failed to set log level %v", err)
 		os.Exit(1)
